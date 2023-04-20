180a5c9e38905d8c2c6c85bb0b6be99021b6c75b
Elijah Mylcev <elijahmylcev@gmail.com>
2023-04-20 18:11:28 +0200

    Created new mixin for buttons  || created new animation for buttons || added function for skills accordion

--- src/js/script.js
+++ src/js/script.js
@@ -4,6 +4,7 @@ const closeElem = document.querySelector('.menu__close');
 const menuOverlay = document.querySelector('.menu__overlay');
 const promo = document.querySelector('.promo');
 const languages = document.querySelectorAll('.select_lang__item');
+const accordionButton = document.querySelector('.skills__more');
 
 function closeMenu() {
     menu.classList.remove('active');
@@ -68,6 +69,27 @@ document.addEventListener('scroll', function () {
     }
 })
 
+// Accordion
+const skills_items = document.querySelector('.skills__items')
+const paddingTop = Number(getComputedStyle(skills_items).paddingTop.slice(0, -2))
+const gridTemplateRows = getComputedStyle(skills_items).getPropertyValue('grid-template-rows')
+const rows = gridTemplateRows.split(' ');
+const heightFirstRow = Number(rows[0].slice(0, -2))
+const heightSecondRow = Number(rows[1].slice(0, -2))
+const rowGap = Number(getComputedStyle(skills_items).rowGap.slice(0, -2))
+skills_items.style.height = paddingTop > 5 ? heightFirstRow + heightSecondRow + rowGap +'px' : heightFirstRow + heightSecondRow + rowGap +'px'
+
+accordionButton.addEventListener('click', () => {
+    if (skills_items.offsetHeight >= skills_items.scrollHeight) {
+        skills_items.style.height = paddingTop > 5 ? heightFirstRow + heightSecondRow + rowGap +'px' : heightFirstRow + heightSecondRow + rowGap +'px'
+        accordionButton.innerText = 'More...'
+    } else {
+        skills_items.style.height = skills_items.scrollHeight+ 2 + 'px'
+        accordionButton.innerText = 'less...'
+    }
+})
+
+
 // Work with languages
 const existsLanguages = ['en', 'ru']
 const changeUrlHash = (e) => {
@@ -82,10 +104,7 @@ const changeLanguage = () => {
     loadLanguage(hash)
 }
 
-// changeLanguage()
-
 languages.forEach(item => item.addEventListener('click', e => changeUrlHash(e.target)))
-
 window.addEventListener('hashchange', changeLanguage)
 
 function loadLanguage(lang) {
@@ -97,8 +116,6 @@ function loadLanguage(lang) {
     xhr.onreadystatechange = function () {
         if (xhr.readyState === 4 && xhr.status === 200) {
             const texts = JSON.parse(xhr.responseText);
-            console.log(texts);
-
             for (let key in texts) {
                 let elem = document.querySelectorAll(`[data-lang-${key}`);
                 if (elem) {
